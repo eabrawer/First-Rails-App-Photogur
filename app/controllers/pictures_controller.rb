6048b51e69b52bc61dbf90bcea1d4cb8e1a73477
Eric Brawer <ericbrawer@gmail.com>
2013-09-06 01:37:00 -0400

    need to ask about html spacing and create new picture

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,42 @@
+class PicturesController < ApplicationController
+	def index
+		@pictures = Picture.all
+	end
+
+	def show
+		@picture = Picture.find(params[:id])
+	end 
+
+	def new
+		@picture = Picture.new 
+	end
+
+	def create
+		@picture = Picture.new(params.permit(:title, :content, :photo, :author))
+
+		if @picture.save
+			redirect_to pictures_url
+		else
+			render :new
+		end
+	end
+
+	def edit
+		@picture = Picture.find(params[:id])
+	end
+
+	def update
+		@picture = Picture.find(params[:id])
+		if @picture.update_attributes(params.require(:picture).permit(:title, :author, :comment, :photo))
+     		redirect_to "/pictures/#{@picture.id}"
+   		else
+   			render :edit
+    	end
+	end
+
+	def destroy
+		@picture = Picture.find(params[:id])
+    	@picture.destroy
+    	redirect_to pictures_url
+	end 
+end
